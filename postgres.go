780b35794cc8d60737b3b2390234f8c95996b784
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2022-05-26 11:52:28 +0700

    Add postgres init and database logic

---
+++ postgres.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/law-a-1/product-service/ent"
+	_ "github.com/lib/pq"
+)
+
+func NewPostgres() (*ent.Client, error) {
+	client, err := ent.Open(
+		"postgres",
+		"host=localhost port=5432 user=postgres dbname=productdb password=root sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
