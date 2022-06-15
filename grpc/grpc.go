5d9e816fee13c3672e940ce8c4e96cfcc25b6c66
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2022-06-16 02:37:10 +0700

    Add grpc init function

---
+++ grpc/grpc.go
@@ -0,0 +1,36 @@
+package grpc
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	grpcServer *grpc.Server
+}
+
+type server struct {
+	UnimplementedProductServer
+}
+
+func NewServer() Server {
+	s := grpc.NewServer()
+	RegisterProductServer(s, &server{})
+	return Server{
+		grpcServer: s,
+	}
+}
+
+func (s *Server) Start() error {
+	lis, err := net.Listen("tcp", ":"+os.Getenv("PORT"))
+	if err != nil {
+		return err
+	}
+	if err := s.grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+	return nil
+}
