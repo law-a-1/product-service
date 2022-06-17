7207e7ff276eb0d76fe7bdbb554f9b3e7b295a51
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2022-06-17 16:53:24 +0700

    Add DecreaseStock GRPC implementation

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -1,6 +1,11 @@
 package grpc
 
 import (
+	"context"
+	"github.com/law-a-1/product-service/ent/product"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"log"
 	"net"
 	"os"
@@ -35,4 +40,31 @@ func (s *Server) Start() error {
 		log.Fatalf("failed to serve: %v", err)
 	}
 	return nil
+}
+
+func (s *Server) DecreaseStock(ctx context.Context, in *DecreaseStockRequest) (*DecreaseStockResponse, error) {
+	p, err := s.db.Product.Query().Where(product.ID(int(in.ID))).First(ctx)
+	if err != nil {
+		if ent.IsNotFound(err) {
+			s.logger.Fatalf("Product not found")
+			st := status.New(codes.NotFound, "Product with given ID not found.")
+			return &DecreaseStockResponse{}, st.Err()
+		}
+		s.logger.Fatalf("")
+		st := status.New(codes.Internal, "Failed to find product with given ID.")
+		return &DecreaseStockResponse{}, st.Err()
+	}
+	if p.Stock < int(in.Amount) {
+		s.logger.Fatalf("")
+		st := status.New(codes.InvalidArgument, "Stock is not enough.")
+		return &DecreaseStockResponse{}, st.Err()
+	}
+	_, err = s.db.Product.Update().SetStock(int(in.Amount)).Save(ctx)
+	if err != nil {
+		s.logger.Fatalf("")
+		st := status.New(codes.Internal, "Failed to update stock data.")
+		return &DecreaseStockResponse{}, st.Err()
+	}
+
+	return &DecreaseStockResponse{}, nil
 }
